751848ca332f7f54e18c9c8a5d0ead05d6dc8bdb
MatyAB <109068011+MatyAB@users.noreply.github.com>
2023-06-01 17:25:41 +0300

    saving student mark ones to attd table databases

--- face_recognition.py
+++ face_recognition.py
@@ -13,6 +13,7 @@ import datetime
 from pyzbar.pyzbar import decode
 import matplotlib.pyplot as plt
 import time
+import tkinter as tk
 
 class Face_Recognition:
 
@@ -21,7 +22,12 @@ class Face_Recognition:
         self.root.geometry("1366x768+0+0")
         self.root.title("Face Recognition Pannel")
 
-        self.var_dep=StringVar()
+        self.var_course=StringVar()
+        self.var_section=StringVar()
+        self.var_lecture = tk.StringVar()
+        self.var_student=StringVar()
+        self.var_recognized_ids = set()  
+
         # This part is image labels setting start 
         # first header image  
         img=Image.open(r"C:\Users\matib\OneDrive\Desktop\MyFinal\Images_GUI\banner.jpg")
@@ -63,79 +69,214 @@ class Face_Recognition:
         std_b1_1 = Button(bg_img,command=self.face_recog,text="Face Detector",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
         std_b1_1.place(x=700,y=350,width=180,height=45)
         # QR-code Button
-        std_b1 = Button(bg_img,command=self.Qr_code,image=self.std_img2,cursor="hand2")
+        std_b1 = Button(bg_img,command=self.mark_attendanceQR,image=self.std_img2,cursor="hand2")
         std_b1.place(x=500,y=170,width=180,height=180)
 
-        std_b1_1 = Button(bg_img,command=self.Qr_code,text="QR-code Scanner",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        std_b1_1 = Button(bg_img,command=self.mark_attendanceQR,text="QR-code Scanner",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
         std_b1_1.place(x=500,y=350,width=180,height=45)
 
           # Current Course 
+           # Current Course 
         current_course_frame = LabelFrame(bd=2,bg="white",relief=RIDGE,text="",font=("verdana",12,"bold"),fg="navyblue")
-        current_course_frame.place(x=100,y=300,width=300,height=100)
+        current_course_frame.place(x=100,y=300,width=350,height=100)
          #label Department
-        dep_label=Label(current_course_frame,text="name csv file",font=("verdana",12,"bold"),bg="white",fg="navyblue")
+        dep_label=Label(current_course_frame,text="Select course",font=("verdana",12,"bold"),bg="white",fg="navyblue")
         dep_label.grid(row=0,column=0,padx=5,pady=15)
 
+        dep_label=Label(current_course_frame,text="Select section",font=("verdana",12,"bold"),bg="white",fg="navyblue")
+        dep_label.grid(row=1,column=0,padx=5,pady=15)
+
+                #combo box 
+            # Connect to the database
+        db = mysql.connector.connect( host="localhost", user="root", password="maty", database="smart_attendance")
+        cursor = db.cursor()
+
+        # Get the course names from the database
+        # cursor.execute("SELECT course_id from assign_course WHERE lecture_id={1221}")
+        cursor.execute("SELECT course_idd FROM assign_course where lecture_id=3")
+        courses = cursor.fetchall()
+        # this code used for fetching lecture id for attendance purpose 
+        cursor.execute("select email from lecture where lecture_id=1")
+        row=cursor.fetchone()
+        # Access the first element of the tuple to get the email value
+        # self.var_lecture = row[0]
+        self.var_lecture.set(row[0])
+        # Create the gender_combo widget
+        self.gender_combo=ttk.Combobox(current_course_frame,textvariable=self.var_course,width=13,font=("verdana",12,"bold"),state="readonly")
+       
+        self.gender_combo.grid(row=1,column=1,padx=5,pady=5,sticky=W)
+
+        # Update the values of the gender_combo widget with the course names
+        values = [course[0] for course in courses]
+        self.gender_combo.config(values=values)
+
+      
         #naming of the file name of attendance
-    
-        current_course_frame = ttk.Entry(current_course_frame,textvariable=self.var_dep,width=15,font=("verdana",12,"bold"))
-        current_course_frame.grid(row=0,column=1,padx=5,pady=5,sticky=W)
-
-        # def create_csv(self):       
-        #     filename="f{d1}.csv"
-        #     filepath= "C:/Users/matib/Videos/prooooo/MY_trial"
-        #     path=os.path.join(filename, filepath)
-        #     with open(filename,"w" ) as csfile:
-
-        #         filewriter=csv.writer(filename, delimiter=',',
-        #                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        gender_combo=ttk.Combobox(current_course_frame,textvariable=self.var_section,width=13,font=("verdana",12,"bold"),state="readonly")
+        gender_combo["values"]=("A","B","C","D","E")
+        gender_combo.current(0)
+        gender_combo.grid(row=0,column=1,padx=5,pady=5,sticky=W)
 
      
+  
 #=====================Attendance==============================
+    # def Qr_code(self):
+      
+    #     with open('./registerStudent.csv', 'r') as f:
+    #         authorized_users = [l[:-1] for l in f.readlines() if len(l) > 2]
+    #         f.close()
 
-
-    def Qr_code(self):
-        # header=['id','roll','name','time','date',]
-        with open('./registerStudent.csv', 'r') as f:
-             authorized_users = [l[:-1] for l in f.readlines() if len(l) > 2]
-             f.close()
-
-        log_path = './log.csv'
-        with open(log_path, 'w') as f:
+    #     log_path = './log.csv'
+    #     with open(log_path, 'w') as f:
                             
-            # f.write("id,roll,name,time,date,attend")
+    #         # f.write("id,roll,name,time,date,attend")
 
+    #         cap = cv2.VideoCapture(0)
+
+    #         most_recent_access = {}
+
+    #         time_between_logs_th = 10
+
+    #         while True:
+
+    #             ret, frame = cap.read()
+
+    #             qr_info = decode(frame)
+
+    #             if len(qr_info) > 0:
+
+    #                 qr = qr_info[0]
+
+    #                 data = qr.data
+    #                 rect = qr.rect
+    #                 polygon = qr.polygon
+
+    #                 if data.decode() in authorized_users:
+    #                     cv2.putText(frame,'ACCESS GRANTED', (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+    #                     if data.decode() not in most_recent_access.keys() \
+    #                         or time.time() - most_recent_access[data.decode()] > time_between_logs_th:
+    #                         most_recent_access[data.decode()] = time.time()
+    #                         with open(log_path, 'a') as f:
+                            
+    #                             f.write(" ")
+    #                             f.write("\n{},{}".format(data.decode(), datetime.datetime.now()))
+    #                             f.close()
+
+    #                         # Create a connection object to the MySQL database file
+    #                         conn = mysql.connector.connect(username='root', password='maty',host='localhost',database='smart_attendance',port=3306)
+                            
+
+    #                         # Create a cursor object that can execute SQL commands
+    #                         cur = conn.cursor()
+
+                            
+                           
+
+    #                         # Insert the data from the csv file into the table
+    #                         cur.execute("INSERT INTO attendance VALUES (%s,%s,%s)", (data.decode(), datetime.datetime.now()))
+
+    #                         # Save the changes to the database
+    #                         conn.commit()
+
+    #                         # Close the connection
+    #                         conn.close()
+
+    #                 else:
+    #                     cv2.putText(frame, 'ACCESS DENIED', (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    #                 frame = cv2.rectangle(frame, (rect.left, rect.top), (rect.left + rect.width, rect.top + rect.height),
+    #                                     (0, 255, 0), 5)
+
+    #                 frame = cv2.polylines(frame, [np.array(polygon)], True, (255, 0, 0), 5)
+
+    #             cv2.imshow('webcam', frame)
+
+    #             if cv2.waitKey(1) & 0xFF == ord('q'):
+    #                 break
+
+    #         cap.release()
+
+
+
+
+    def mark_attendanceQR(self):
+        """Mark the attendance of a student in a course."""
+        section = self.var_section.get()
+        lecture = self.var_lecture.get()
+        course=self.var_course.get()
+        time1 = datetime.datetime.now().strftime("%H:%M:%S")
+        date = datetime.datetime.now().strftime("%d/%m/%Y")
+        
+        # Read the authorized users from the csv file
+        with open("./registerStudent.csv", "r") as f:
+            authorized_users = [l[:-1] for l in f.readlines() if len(l) > 2]
+            f.close()
+        # Open the log file for writing
+        log_path = './log.csv'
+        with open(log_path, 'a') as f:
+
+            # Capture video from the camera
             cap = cv2.VideoCapture(0)
 
+            # Keep track of the most recent access time for each user
             most_recent_access = {}
 
+            # Set a threshold for logging time between accesses
             time_between_logs_th = 10
 
+            # Loop until the user presses q
             while True:
 
+                # Read a frame from the camera
                 ret, frame = cap.read()
 
+                # Decode the QR info from the frame
                 qr_info = decode(frame)
 
+                # If there is any QR info, process it
                 if len(qr_info) > 0:
 
+                    # Get the data, rect and polygon from the first QR info
                     qr = qr_info[0]
-
                     data = qr.data
                     rect = qr.rect
                     polygon = qr.polygon
-
+                    conn = mysql.connector.connect(username="root", password="maty", host="localhost", database="smart_attendance", port=3306)
+                    cur = conn.cursor()
+                    # Check if the data is in the authorized users list
                     if data.decode() in authorized_users:
-                        cv2.putText(frame,'ACCESS GRANTED', (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-                        if data.decode() not in most_recent_access.keys() \
-                                or time.time() - most_recent_access[data.decode()] > time_between_logs_th:
+                        # Display a green message on the frame
+                        cv2.putText(frame, "ACCESS GRANTED", (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+                        # Check if the data is not in the most recent access dict or if the time difference is greater than the threshold
+                        if data.decode() not in most_recent_access or time.time() - most_recent_access[data.decode()] > time_between_logs_th:
+                            # Update the most recent access time for the data
                             most_recent_access[data.decode()] = time.time()
+                            id, name, section = data.decode().split(",")
+                            # Write a log entry to the file with the data and datetime
                             with open(log_path, 'a') as f:
-                            
-                                f.write(" ")
-                                f.write("\n{},{}".format(data.decode(), datetime.datetime.now()))
-                                f.close()
+                
+                                    f.write(" ")
+                                    f.write("\n{},{}".format(data.decode(), datetime.datetime.now()))
+                                    f.close()
+                                    cur.execute("select student_id from std_enroll where student_id=%s AND course_id=%s", (id, course))
+                            # Fetch one row from the cursor as a tuple
+                                    row = cur.fetchone()
+                                    cur.execute("INSERT INTO attd VALUES (null,%s,%s,%s,%s,%s,%s,%s,%s)", (name, id, lecture, course, section, date, time1, "Present"))
+                                # Save the changes to the database
+                                    conn.commit()
+                                    conn.close()
 
+                            
+                           
+                            # Connect to the MySQL database
+                            # Create a cursor object that can execute SQL commands
+                            
+                            
+                            # Check if the student is enrolled in the course using parentheses and AND operator
+
+                            # Check if the row is not None
+                           
+                           
                     else:
                         cv2.putText(frame, 'ACCESS DENIED', (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
@@ -144,32 +285,96 @@ class Face_Recognition:
 
                     frame = cv2.polylines(frame, [np.array(polygon)], True, (255, 0, 0), 5)
 
-                cv2.imshow('webcam', frame)
+                # Display the frame on a window named "QR Code Scanner"
+                cv2.imshow("QR Code Scanner", frame)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                # Wait for 1 millisecond for a key press
+                key = cv2.waitKey(1)
+
+                # If the key is q, break out of the loop
+                if key == ord("q"):
                     break
 
+            # Release the camera resource
             cap.release()
 
-    def mark_attendance(self,i,n,r,filename):           
-        with open(f"./att_reco/{filename}.csv","r+",newline="\n") as f:
+            # Destroy all windows
+            cv2.destroyAllWindows()
+
+   
+    # def mark_attendance(self,i,n,r,course,student_id): 
+    #     section=self.var_section.get()
+    #     lecture=self.var_lecture.get()
+               
+    #     with open(f"./att_reco/{course}.csv","r+",newline="\n") as f:
+    #         myDatalist=f.readlines()
+    #         name_list=[]
+    #         for line in myDatalist:
+    #            entry=line.split((","))
+    #            name_list.append(entry[0])
+
+    #         if((i not in name_list)) and ((r not in name_list)) and ((n not in name_list)):
+    #           now1=datetime.datetime.now()
+    #           d1=now1.strftime("%d/%m/%Y")
+    #           dtString=now1.strftime("%H:%M:%S")
+    #           f.writelines(f" {n},{student_id},{lecture},{course}, {section}, {d1}, {dtString}, Present\n")
+
+    #           conn = mysql.connector.connect( host="localhost", user="root", password="maty", database="smart_attendance")
+            
+    #           cur = conn.cursor()
+  
+    #             # Execute the query to check if the student is registered for the course
+    #           cur.execute("select student_id from std_enroll where student_id=%s AND course_id=%s", (student_id, course))
+             
+             
+    #         if cur.fetchone():
+    #           cur.execute("INSERT INTO attd VALUES (null,%s,%s,%s,%s,%s,%s,%s,%s)", (n,student_id,lecture,course,section,d1,dtString, "Present"))
+    #           self.var_recognized_ids.add(student_id)
+               
+    #         conn.commit()
+
+    #         conn.close()
+
+# Create a set to store the recognized student IDs
+
+
+    def mark_attendance(self,i,n,r,course,student_id): 
+        section=self.var_section.get()
+        lecture=self.var_lecture.get()          
+        with open(f"./att_reco/{course}.csv","r+",newline="\n") as f:
             myDatalist=f.readlines()
-            name_list=[]
+            id_list=[]
             for line in myDatalist:
-               entry=line.split((","))
-               name_list.append(entry[0])
+                entry=line.split((","))
+                id_list.append(entry[1]) # Assuming the second column contains the student IDs
 
-            if((i not in name_list)) and ((r not in name_list)) and ((n not in name_list)):
-              now1=datetime.datetime.now()
-              d1=now1.strftime("%d/%m/%Y")
-              dtString=now1.strftime("%H:%M:%S")
-              f.writelines(f"{i}, {n}, {r}, {dtString}, {d1},{filename}, Present\n")
+            if((student_id not in id_list)) and ((student_id not in self.var_recognized_ids)):
+                now1=datetime.datetime.now()
+                d1=now1.strftime("%d/%m/%Y")
+                dtString=now1.strftime("%H:%M:%S")
+                f.writelines(f" {n},{student_id},{lecture},{course}, {section}, {d1}, {dtString}, Present\n")
 
-    #==============================Face_Recognition==============================
+                conn = mysql.connector.connect( host="localhost", user="root", password="maty", database="smart_attendance")
+                
+                cur = conn.cursor()
+    
+                # Execute the query to check if the student is registered for the course
+                cur.execute("select student_id from std_enroll where student_id=%s AND course_id=%s", (student_id, course))
+                
+                # Check if the query returns any rows
+                if cur.fetchone():
+                    # Insert the data into the attd table
+                    cur.execute("INSERT INTO attd VALUES (null,%s,%s,%s,%s,%s,%s,%s,%s)", (n,student_id,lecture,course,section,d1,dtString, "Present"))
+                    # Add the student ID to the recognized set
+                    self.var_recognized_ids.add(student_id)
+                
+                conn.commit()
+
+                conn.close()
 
     def face_recog(self):
-        filename=self.var_dep.get()
-        with open(f"./att_reco/{filename}.csv",'w') as file:
+        course=self.var_course.get()
+        with open(f"./att_reco/{course}.csv",'w') as file:
          writer=csv.writer(file)
         def draw_boundray(img,classifier,scaleFactor,minNeighbors,color,text,clf):
             gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -186,6 +391,7 @@ class Face_Recognition:
                 cursor.execute("select name from student where student_id="+str(id))
                 n=cursor.fetchone()
                 n="+".join(n)
+                student_id=id
 
                 cursor.execute("select section from student where student_id="+str(id))
                 r=cursor.fetchone()
@@ -196,11 +402,11 @@ class Face_Recognition:
                 i="+".join(i)
 
 
-                if confidence > 75:
+                if confidence >80 :
                     cv2.putText(img,f"year:{i}",(x,y-80),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
                     cv2.putText(img,f"name:{n}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
                     cv2.putText(img,f"section:{r}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(64,15,223),2)
-                    self.mark_attendance(i,n,r,filename)
+                    self.mark_attendance(i,n,r,course,student_id)
                 else:
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
                     cv2.putText(img,"Unknown Face",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,0),3)    
@@ -210,7 +416,8 @@ class Face_Recognition:
             return coord    
 
 
-        #=============================
+        #==========================================================
+
         def recognize(img,clf,faceCascade):
             coord=draw_boundray(img,faceCascade,1.1,10,(255,25,255),"Face",clf)
             return img
@@ -227,12 +434,10 @@ class Face_Recognition:
             cv2.imshow("Face Detector",img)
 
 
-
             if cv2.waitKey(1) & 0xFF == ord('q') :
                 break
         videoCap.release()
         cv2.destroyAllWindows()
-
 
 
 
